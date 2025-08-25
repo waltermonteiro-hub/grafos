c01a321719b77bda19b02ce23c90cacacdabf713
betotorres <betotorres@users.noreply.github.com>
2025-08-25 19:37:17 -0300

    Update matriz.py

--- src/matriz.py
+++ src/matriz.py
@@ -121,3 +121,163 @@ class GrafoDenso(Grafo):
         for i, linha in enumerate(self.matriz):
             print(f"{self.labels[i]} |", "  ".join(map(str, linha)))
         print()
+
+
+class GrafoEsparso(Grafo):
+    """
+    Implementa as operações básicas de um grafo não orientado
+    usando uma LISTA DE ADJACÊNCIAS (implementada com um dicionário).
+    """
+    # (i) Definição do grafo
+    def __init__(self, num_vertices=None, labels=None):
+
+        if labels:
+            self.vertices = labels
+        elif num_vertices:
+            self.vertices = [str(i) for i in range(num_vertices)]
+        else:
+            print("Erro: Forneça 'num_vertices' ou uma lista de 'labels'.")
+            sys.exit(1)
+
+        # A lista de adjacências é um dicionário onde cada vértice
+        # é uma chave e o valor é a lista de seus vizinhos.
+        self.lista_adj = {vertice: [] for vertice in self.vertices}
+
+  
+    def numero_de_vertices(self):
+        # Retorna o número total de vértices no grafo.
+        return len(self.vertices)
+
+    def numero_de_arestas(self):
+        # Retorna o número total de arestas no grafo.
+        return int(sum([len(vizinhos) for vizinhos in self.lista_adj.values()])/2)
+
+
+    def sequencia_de_graus(self):
+        # Retorna uma lista com os graus de todos os vértices.
+        return sorted([len(values) for values in self.lista_adj.values()])
+
+
+
+
+    def _validar_vertice(self, vertice):
+        """Método auxiliar para checar se um vértice existe no grafo."""
+        if vertice not in self.lista_adj:
+            raise ValueError(f"Vértice '{vertice}' não existe no grafo.")
+        return True
+
+    # (ii) Adição de arestas
+    def adicionar_aresta(self, u, v):
+        try:
+            self._validar_vertice(u)
+            self._validar_vertice(v)
+
+            # Pode adicionar aresta duplicada ou laço (loop)
+            self.lista_adj[u].append(v)
+            self.lista_adj[v].append(u)
+ 
+            print(f"Aresta adicionada entre {u} e {v}")
+        except ValueError as e:
+            print(f"Erro ao adicionar aresta: {e}")
+
+
+    # (v) Remoção de arestas
+    def remover_aresta(self, u, v, peso=None):
+        """
+        Se existir mais de uma, remove a primeira aresta entre os vértices u e v.
+        """
+        try:
+            self._validar_vertice(u)
+            self._validar_vertice(v)
+
+            for index, ver in enumerate(self.lista_adj[u]):
+                if v == ver:
+                    del self.lista_adj[u][index]
+                    print(f"Aresta removida entre {u} e {v}.")
+                    break
+            else:
+                print(f"Aresta entre {u} e {v} não existe.")
+
+            for index, ver in enumerate(self.lista_adj[v]):
+                if u == ver:
+                    del self.lista_adj[v][index]
+                    print(f"Aresta removida entre {v} e {u}.")
+                    break
+            else:
+                print(f"Aresta entre {u} e {v} não existe.")
+
+        except ValueError as e:
+            print(f"Erro ao remover aresta: {e}")
+
+
+
+    def imprimir(self):
+        """Imprime a lista de adjacências de forma legível."""
+        print("\nLista de Adjacências:")
+        if not self.lista_adj:
+            print("{}")
+            return
+        for vertice, vizinhos in self.lista_adj.items():
+            # Junta a lista de vizinhos em uma string para impressão
+            saida = [vizinho for vizinho in vizinhos]             
+            print(f"  {vertice} -> [ {saida} ]")
+        print()
+
+
+
+if __name__ == "__main__":
+    
+    vertices_labels = ['A', 'B', 'C', 'D', 'E']
+    #g = GrafoDenso(labels=vertices_labels)
+    g = GrafoEsparso(labels=vertices_labels)
+  
+    g.adicionar_aresta('A', 'B')
+    g.adicionar_aresta('A', 'C')
+    g.adicionar_aresta('A', 'C')
+    g.adicionar_aresta('C', 'D')
+    g.adicionar_aresta('C', 'E')
+    g.adicionar_aresta('B', 'D')
+    g.imprimir()
+    print(f"Número de vértices: {g.numero_de_vertices()}")
+    print(f"Número de arestas: {g.numero_de_arestas()}")
+    print(f"Sequência de graus: {g.sequencia_de_graus()}")
+    g.remover_aresta('A', 'C')
+    g.imprimir()
+
+
+    """"
+
+    vertices_labels = ['A', 'B', 'C', 'D', 'E']
+    g = GrafoDenso(labels=vertices_labels)
+
+    print("Grafo inicial criado.")
+    g.imprimir()
+
+    # (ii) Adição de arestas
+    print("\n--- Adicionando arestas ---")
+    g.adicionar_aresta('A', 'B')
+    g.adicionar_aresta('A', 'C')
+    g.adicionar_aresta('B', 'A')
+    g.adicionar_aresta('B', 'E')
+    g.adicionar_aresta('C', 'A')
+    g.imprimir()
+    g.remover_aresta('A', 'C')
+    g.imprimir()
+
+    print(g.numero_de_arestas())
+  
+
+    g = GrafoDenso(num_vertices=5)
+    print("Grafo inicial criado.")
+    g.imprimir()
+    g.adicionar_aresta(1, 2)
+    g.adicionar_aresta(1, 3)
+    g.adicionar_aresta(0, 4)
+    g.adicionar_aresta(1, 4)
+    g.adicionar_aresta(2, 3)
+    g.imprimir()
+    print(g.numero_de_vertices())
+    print(g.numero_de_arestas())
+    print(g.sequencia_de_graus())
+
+"""
